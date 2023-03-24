958f7a3061f4bb34564d57fb2eef2e6b76e33c0e
Gurvinder Nagra (nagrag4) <nagrag4@coventry.ac.uk>
2023-03-24 09:22:56 +0000

    Adding models and services

---
+++ GymTrainer Project/Services/MyDayService.cs
@@ -0,0 +1,41 @@
+﻿using GymTrainer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymTrainer.Services
+{
+    /// <summary>
+    /// This service will handle HTTP requests to retrieve data from the cloud
+    /// </summary>
+    public class MyDayService
+    {
+        HttpClient httpClient;
+        List<Day> days = new();
+
+        public MyDayService()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public async Task<List<Day>> GetDays()
+        {
+            if (days?.Count > 0)
+                return days;
+
+            var url = "https://gist.githubusercontent.com/Gurvinder-007/ebd06f6ac2d9aa8ec1bef84cfae64b42/raw/d85ad7c46dc2baf0514dae6e5a21688b61932190/Gym.json";
+
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                days = await response.Content.ReadFromJsonAsync<List<Day>>();
+            }
+
+            return days;
+        }
+    }
+}
---
+++ GymTrainer Project/ViewModels/MyDaysViewModel.cs
@@ -0,0 +1,73 @@
+﻿using GymTrainer.Models;
+using GymTrainer.Services;
+using static Android.Telephony.CarrierConfigManager;
+
+namespace GymTrainer.ViewModels
+{
+    public partial class MyDaysViewModel : BaseViewModel
+    {
+        MyDayService myDayService;
+
+        // ObservableCollection is used since it has built-in support to raise CollectionChanged
+        // OnPropertyChanged does not need to be called
+        public ObservableCollection<Day> MyDays { get; } = new();
+
+        // Inject MyDayService through the constructor
+        public MyDaysViewModel(MyDayService myDayService)
+        {
+            Title = "Workout Scheduler";
+            this.myDayService = myDayService;
+        }
+
+        // Generates GoToDetailsCommand automatically
+        [RelayCommand]
+        async Task GoToDetailsAsync(Day day)
+        {
+            if (day is null)
+                return;
+
+            // Use the built-in Shell Navigation API
+            // When item is selected, push a new DetailsPage with the day as the parameter
+            await Shell.Current.GoToAsync($"{nameof(DetailsPage)}", 
+                true,
+                new Dictionary<string, object>
+                {
+                    // Query identifier
+                    {nameof(Day), day}
+                });
+        }
+
+        // RelayCommand enables us to bind the command and data between the viewmodel and view
+        // Generates GetMyDaysCommand automatically
+        [RelayCommand]
+        async Task GetMyDaysAsync()
+        {
+            if (IsBusy)
+                return;
+
+            // Toggle IsBusy to true when making calls to the server
+            // False when finished with the call to the server
+            try
+            {
+                IsBusy = true;
+
+                var days = await myDayService.GetDays();
+
+                if(MyDays.Count != 0)
+                    MyDays.Clear();
+
+                foreach(var day in days)
+                    MyDays.Add(day);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await Shell.Current.DisplayAlert("Error!", "Unable to get days.", "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
